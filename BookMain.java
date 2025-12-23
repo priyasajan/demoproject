383bd6087f1b1837e24cde36d92dc0a95abfbd03
Priya Sajan <priyasajan1414@gmail.com>
2025-12-23 15:12:24 +0530

    orikkal bookname chnged to orikkals

--- BookMain.java
+++ BookMain.java
@@ -1,7 +1,7 @@
 public class BookMain{
 public static void main (String []args){
 
-Book mybook = new Book( "Orikkal", "V.D", 150);
+Book mybook = new Book( "Orikkals", "V.D", 150);
 
 mybook.displayDetails();
 }
